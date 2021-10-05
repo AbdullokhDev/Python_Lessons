7a1fa80cd9c035017347c285350a358c9dfbc07b
AbdullokhDev <abdulloh.watch@internet.ru>
2021-10-05 22:10:18 +0500

    exercises do not completed yet

--- Lesson.15/ClassEx.py
+++ Lesson.15/ClassEx.py
@@ -1,50 +1,41 @@
-# products = {
-#     'apple': 10000,
-#     'pomegranate': 20000,
-#     'grape': 40000,
-#     'apricot': 30000}
+# #Ex1:
+# pythonDictionary = {
+#     'if' : 'statement returns true or false',
+#     'tuple' : 'used to store multiple items in a single variable',
+#     'list' : 'used for changable list',
+#     'for' : 'used for looping the function',
+#     '%' : 'modular division is used to find remainder',
+#     'array' : 'used to save many values of the the same data',
+#     'dictionary' : 'used for store the data',
+#     }
+# print("List: ")
+# for a, b in sorted(pythonDictionary.items()):
+#     print(f"{a.title()} means that {b.upper()}")
 
-# # print(products.keys())
+# #Ex2:
+countries = {
+    'Uzbekistan' : 'Tashkent',
+    'USA' : 'washington D.C',
+    'Argentina' : 'Buenos Aires',
+    'Australia' : 'Canberra',
+    'Colombia' : 'Bogota',
+    'Japan' : 'Tokyo',
+    'Germany' : 'Berlin',
+    'Egypt' : 'Cairo',
+    'United Kingdom	' : 'London',
+    'Norway' : 'Oslo'
+    }
+# print('Countries of the world: ')
+# for country in sorted(countries):
+#     print(country.upper())
+# print("The capitals of the countries: ")
+# for capital in sorted(countries.values()):
+#     print(capital)
 
-# print("Products that can be found in our store: ")
-# for product in products.keys():
-#     print(product.title())
-
-# products = {
-#     'apple': 10,
-#     'pomegranate': 22,
-#     'grape': 40,
-#     'apricot': 31}
-# listToPurchase = ['bread', 'fish', 'apricot', 'grape', 'egg']
-# print("Products that can be found in our store:")
-# for product in products:
-#     if product in listToPurchase:
-#         print(f"{product.title()} {products[product]} dollar")
-# for item in listToPurchase:
-#     if item not in products:
-#         print(f"Please, bring {item.title()} product")
-
-# products = {
-#     'apple': 10,
-#     'pomegranate': 22,
-#     'grape': 40,
-#     'apricot': 31}
-# # print("Products in our store: ")
-# # for product in sorted(products):
-# #     print(product.title())
-# print(products.values())
-
-smartphones = {
-    'abdullokh':'iphone xr',
-    'amir' : 'iphone xs max',
-    'shakhida' : 'mi9',
-    'abror':'galaxy s10 plus',
-    'said':'oneplus 8pro',
-    'ali':'iphone xr',
-    'hojiakbar':'mi9'}
-print("Users are using these phones: ")
-# for smartphone in smartphones.values():
-#     print(smartphone)
-for smartphone in set(smartphones.values()):
-        print(smartphone)
-
+#Ex3:
+countryName = input("Enter any country you would like to visit: ").lower()
+capital = countries.get(countryName)
+if capital == None:
+    print("Sorry we does not offer for this country")
+else:
+    print(f"Congratulations, you can go {countryName.title()} country. It's capital is {capital.title()}")
