d7a2b55c19c83fb2caf37bdcd7a4315747d49da8
Abdullokh <91453062+AbdullokhDev@users.noreply.github.com>
2021-10-29 15:34:14 +0500

    updated

--- findingTheNumb/game.py
+++ findingTheNumb/game.py
@@ -5,18 +5,29 @@ Created on Fri Oct 29 08:47:07 2021
 @author: Abdulloh
 """
 
-import random as r
-
+import random
 
 print('\nFinding the number game is started>>>')
 
-number = []
-getValue = r.sample(range(10), 1)
-print(getValue)
-
-print('I thinked the number')
-
-def pcNumber(x):
-    guess = input("Guess the number: ")
+def findingNumbFunc(x=10):
+    generatedNumb = random.randint(1, x)
+    print(f"I think number from 1 to {x}. Can you find it?")
+    guesses = 0
     while True:
+        guesses += 1
+        guessedNumb = int(input('\n>>>: '))
+        if guessedNumb < generatedNumb:
+            print('I though bigger number then you entered! Try again!')
+        elif guessedNumb > generatedNumb:
+            print('I though smaller number then you entered! Try again!')
+        else:
+            break
+    print(f"Congrats. You found it in {guesses} try!")
+    return guesses
+
+def findMyNumbFunc(y=10):
+    generatedNumb = random.randint(1,y)
+    
+    while True:
+        sign = input("")
         
