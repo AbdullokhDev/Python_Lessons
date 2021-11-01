6f45a173b5b804c2c8abf636d0b19d9766674e69
Abdullokh <91453062+AbdullokhDev@users.noreply.github.com>
2021-11-01 11:28:35 +0500

    finally

--- findingTheNumb/game.py
+++ findingTheNumb/game.py
@@ -25,9 +25,39 @@ def findingNumbFunc(x=10):
     print(f"Congrats. You found it in {guesses} try!")
     return guesses
 
-def findMyNumbFunc(y=10):
-    generatedNumb = random.randint(1,y)
-    
+def findPC(x=10):
+    input(f"Think any number from 1 to {x} and press any keyboard to continue: ")
+    smallest = 1
+    biggest = x
+    tries = 0
     while True:
-        sign = input("")
+        tries += 1
+        if smallest != biggest:
+            guess = random.randint(smallest, biggest)
+        else:
+            guess = smallest
+        answer = input(f"You think {guess} number if it is correct, please write (T),"
+                  f" or the number is bigger than this put (+) sign or it is lower than this put (-) sign ".lower())
+        if answer == "-":
+            biggest = guess - 1
+        elif answer == "+":
+            smallest = guess + 1
+        else:
+            break
+    print(f"I found it in {tries} try!")
+    return tries
+
+
+def play(x=10):
+    againPlay = True
+    while againPlay:
+        guesses_user = findingNumbFunc(x)
+        guesses_pc = findPC(x)
         
+        if guesses_user > guesses_pc:
+            print("I win")
+        elif guesses_user < guesses_pc:
+            print("You win")
+        else:
+            print("Equal!")
+        againPlay = int(input(("Do you want to play again? if yes type (1) else type (0) ")))
