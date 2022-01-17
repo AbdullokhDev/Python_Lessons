581cf47298bfef07246cf8dbf5947572e60dcbf2
Abdullokh <abdulloh.watch@internet.ru>
2022-01-17 16:29:31 +0500

    done

---
+++ Lesson_19/function.py
@@ -0,0 +1,32 @@
+# def say_hello(name):
+#     """
+#         function says hello
+        
+#         first it stores user's name
+#         then, say hello {user!}
+#     """
+    
+#     print(f"Hello, dear {name.title()}!")
+    
+# say_hello("nuriddin")
+
+# def full_name(first_name, last_name):
+#     """
+#         functions gives full name of a user
+#     """
+    
+#     print(f"User's first name is: {first_name.title()}\n"
+#           f"User's last name is: {last_name.title()}")
+    
+# full_name("abdullokh", "abdumannopov")
+
+def calc_age(name, dob):
+    """
+        Calculates age of user,
+        and give his/her name
+    """
+    print(f"User's first name is: {name.title()}\n"
+          f"Age is: {2022-dob}")
+    
+calc_age("abdullokh", 1999)
+
